b3293c81d547df957b2f7d0cb625a17e29f73307
Shivani Charhate <shivani.charhate@xebia.com>
2026-05-16 20:41:08 +0530

    setup for backend and frontend

---
+++ client/src/assets/assets.js
@@ -0,0 +1,101 @@
+import arrow_icon from './arrow_icon.png'
+import avatar_icon from './avatar_icon.png'
+import bgImage from './bgImage.png'
+import code from './code.png'
+import gallery_icon from './gallery_icon.png'
+import help_icon from './help_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import logo_big from './logo_big.png'
+import logo_icon from './logo_icon.png'
+import logo from './logo.png'
+import menu_icon from './menu_icon.png'
+import pic1 from './pic1.png'
+import pic2 from './pic2.png'
+import pic3 from './pic3.png'
+import pic4 from './pic4.png'
+import profile_alison from './profile_alison.png'
+import profile_enrique from './profile_enrique.png'
+import profile_marco from './profile_marco.png'
+import profile_martin from './profile_martin.png'
+import profile_richard from './profile_richard.png'
+import search_icon from './search_icon.png'
+import send_button from './send_button.png'
+export const assets = { arrow_icon, avatar_icon, bgImage, code, gallery_icon, help_icon, img1, img2, logo_big, logo_icon, logo, menu_icon, pic1, pic2, pic3, pic4, profile_alison, profile_enrique, profile_marco, profile_martin, profile_richard, search_icon, send_button }
+export const imagesDummyData = [pic1, pic2, pic3, pic4, pic1, pic2]
+export const userDummyData = [
+    {
+        "_id": "6749nghkf748490",
+        "email": "test@gmail.com",
+        "full Name": "Marco Jones",
+        "profilePic": profile_marco,
+        "bio": "Hi Everyone, I am using quick chat"
+
+    },
+    {
+        "_id": "6749nghkf748499",
+        "email": "test1@gmail.com",
+        "full Name": "Alison Marco",
+        "profilePic": profile_alison,
+        "bio": "Hi Everyone, I am using quick chat its amazing"
+
+    },
+    {
+        "_id": "6749nghkf748492",
+        "email": "test2@gmail.com",
+        "full Name": "Enrique Smith",
+        "profilePic": profile_enrique,
+        "bio": "Hi Everyone, I am using quick chat its free"
+
+    },
+    {
+        "_id": "6749nghkf748480",
+        "email": "test3@gmail.com",
+        "full Name": "Martin Wick",
+        "profilePic": profile_martin,
+        "bio": "Hi Everyone, I am using quick chat "
+
+    }
+]
+export const messagesDummyData = [
+    {
+        "_id": "6749nghkf748492hgvh90",
+        "senderId": "6749nghkf748492",
+        "receiverId": "6749nghkf748492uyu67",
+        "text": "format that includes user records and their respective timestamps.",
+        "seen": "true",
+        "createdAt": "2026-05-14T08:30:15Z",
+    },
+    {
+        "_id": "6749nghkf748492hgvh70",
+        "senderId": "6749nghkf748452",
+        "receiverId": "6749nghkf748492uyu37",
+        "text": "format that includes user records and their respective timestamps.",
+        "seen": "true",
+        "createdAt": "2026-05-10T08:30:15Z",
+    },
+    {
+        "_id": "6749nghkf748492hgerh90",
+        "senderId": "6749nghkf787492",
+        "receiverId": "6749nghkf748492uyu09",
+        "text": "format that includes user records and their respective timestamps.",
+        "seen": "true",
+        "createdAt": "2026-05-11T08:30:15Z",
+    },
+    {
+        "_id": "6749nghkf709492hgvh90",
+        "senderId": "6749nghkf74io92",
+        "receiverId": "6749nghkf748490uyu67",
+        "text": "format that includes user records and their respective timestamps.",
+        "seen": "true",
+        "createdAt": "2026-05-09T08:30:15Z",
+    },
+    {
+        "_id": "6749nghkf748492hgvh60",
+        "senderId": "6749nghkf74849289",
+        "receiverId": "6749nghkf748492uyu609",
+        "text": "Here is a clean, formatted sample of dummy data in JSON format that includes user records and their respective timestamps.",
+        "seen": "true",
+        "createdAt": "2026-05-07T08:30:15Z",
+    },
+]
